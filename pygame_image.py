d9f900165707139e7a4f4c4e840f91d5900f30a5
kazuya <c0b230742b@edu.teu.ac.jp>
2024-06-11 16:50:10 +0900

    練習8：基礎終了

--- pygame_image.py
+++ pygame_image.py
@@ -13,6 +13,8 @@ def main():
     bg_img2 = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png")
     kk_img =pg.transform.flip(kk_img, True, False)
+    kk_rect = kk_img.get_rect() #こうかとんRectの抽出
+    kk_rect.center = 300, 200
     tmr = 0
     width = 0
     while True:
@@ -25,8 +27,18 @@ def main():
         screen.blit(bg_img2, [-width+1600, 0])
         screen.blit(bg_img, [-width+3200, 0])
         screen.blit(bg_img2, [-width+4800, 0])
-        kk_rect = kk_img.get_rect() #こうかとんRectの抽出
-        kk_rect.center = 300, 200
+
+        key_lst = pg.key.get_pressed()
+
+        if key_lst[pg.K_UP]:
+            kk_rect.move_ip(0, -1)
+        elif key_lst[pg.K_DOWN]:
+            kk_rect.move_ip(0, 1)
+        elif key_lst[pg.K_RIGHT]:
+            kk_rect.move_ip(1, 0)
+        elif key_lst[pg.K_LEFT]:
+            kk_rect.move_ip(-1, 0)
+    
         screen.blit(kk_img, kk_rect) #kk_imgをkk_rectの設定に従って貼り付け
         pg.display.update()
         tmr += 1        
