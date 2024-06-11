3c1d2450ac31b38de90834906860fe6a17f86453
kazuya <c0b230742b@edu.teu.ac.jp>
2024-06-11 16:37:25 +0900

    練習7：はいけいへんしゅう

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png")
     kk_img =pg.transform.flip(kk_img, True, False)
     tmr = 0
@@ -18,9 +19,12 @@ def main():
         width += 1
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        width = tmr%800
+        width = tmr%3200
 
         screen.blit(bg_img, [-width, 0])
+        screen.blit(bg_img2, [-width+1600, 0])
+        screen.blit(bg_img, [-width+3200, 0])
+        screen.blit(bg_img2, [-width+4800, 0])
         kk_rect = kk_img.get_rect() #こうかとんRectの抽出
         kk_rect.center = 300, 200
         screen.blit(kk_img, kk_rect) #kk_imgをkk_rectの設定に従って貼り付け
